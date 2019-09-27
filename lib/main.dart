188d29b2b47f1f54d06b66b1fcc373ced5bb56b6
mabirizi <mabiriziad@gmail.com>
2019-09-27 11:31:09 +0300

    using the double.infinity as width

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,29 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: Text('CHART'),
-          ),
-          Card(child: Text('List of TX'),),
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+          
+                child: Container(
+                  width: double.infinity,
+                  child: Text('CHART'),
+                ),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text('List of TX'),
+            ),
+          ],
+        ));
   }
 }
-
