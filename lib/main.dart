3a11fa976b4dcc391e06866eb2ff00c2d801b0ef
mabirizi <mabiriziad@gmail.com>
2019-09-30 12:52:47 +0300

    Styling the lists of the mapped data

--- lib/main.dart
+++ lib/main.dart
@@ -53,17 +53,32 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: transactions.map((tx) {
                 return Card(
+                  
                     child: Row(
                   children: <Widget>[
                     Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
                       child: Text(
                         tx.amount.toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tx.title),
-                        Text(tx.date.toString()),
+                        Text(tx.title,
+                            style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),
+                        ),
+                        Text(tx.date.toString(),
+                        style: TextStyle(fontSize: 10, color: Colors.grey),),
                       ],
                     )
                   ],
