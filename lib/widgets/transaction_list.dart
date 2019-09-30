e2be6d3176abfd6df7915e628943c0be993733cc
mabirizi <mabiriziad@gmail.com>
2019-09-30 18:31:59 +0300

    woking with list views

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,53 +3,51 @@ import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-
 class TransactionList extends StatelessWidget {
-  
   final List<Transaction> transactions;
   TransactionList(this.transactions);
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      child: SingleChildScrollView(
-              child: Column(
-          children: transactions.map((tx) {
-            return Card(
-                child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 2),
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    tx.amount.toString() + '/=',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple),
-                  ),
+      height: 250,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+              child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tx.title,
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(tx.date),
-                      style: TextStyle(fontSize: 10, color: Colors.grey),
-                    ),
-                  ],
-                )
-              ],
-            ));
-          }).toList(),
-        ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  transactions[index].amount.toString() + '/=',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    transactions[index].title,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                    style: TextStyle(fontSize: 10, color: Colors.grey),
+                  ),
+                ],
+              )
+            ],
+          ));
+        },
+        itemCount: transactions.length,
+        
       ),
     );
   }
