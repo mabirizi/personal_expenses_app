6c630d60f785666e1c783aa2737035aa1cc1ada8
mabirizi <mabiriziad@gmail.com>
2019-10-01 20:17:41 +0300

    finishing the chart bars

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -24,8 +24,8 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E().format(weekDay));
-      print(totalSum);
+      // print(DateFormat.E().format(weekDay));
+      // print(totalSum);
 
       return {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
@@ -35,7 +35,7 @@ class Chart extends StatelessWidget {
   }
 
   double get totalSpending {
-    return groupedTransactionValues.fold(0.0, (sum, item){
+    return groupedTransactionValues.fold(0.0, (sum, item) {
       return sum + item['amount'];
     });
   }
@@ -45,12 +45,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          //return Text('${data['day']}: ${data['amount']}');
-          return ChartBar(data['day'], data['amount'],
-              totalSpending == 0.0 ? 0.0 :(data['amount'] as double) / totalSpending);
-        }).toList(),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            //return Text('${data['day']}: ${data['amount']}');
+            return Flexible(
+              fit: FlexFit.tight,
+                        child: ChartBar(
+                  data['day'],
+                  data['amount'],
+                  totalSpending == 0.0
+                      ? 0.0
+                      : (data['amount'] as double) / totalSpending),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
