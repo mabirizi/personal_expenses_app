67ccceae4f4556ef77e9e846191f4fea0594f47e
mabirizi <mabiriziad@gmail.com>
2019-10-03 17:12:52 +0300

    using keys

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 import '../models/transaction.dart';
 import './transaction_item.dart';
 
@@ -12,31 +11,34 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? LayoutBuilder(builder: (ctx, constraints){
-          return  Column(
-            children: <Widget>[
-              Text(
-                'No transactions yet',
-                style: Theme.of(context).textTheme.title,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                  height: constraints.maxHeight * 0.6,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  )),
-            ],
-          );
-        },)
-        : ListView.builder(
-            itemBuilder: (ctx, index) {
-              return TransactionItem(transaction: transactions[index], deleteTx: deleteTx);
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  Text(
+                    'No transactions yet',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                      height: constraints.maxHeight * 0.6,
+                      child: Image.asset(
+                        'assets/images/waiting.png',
+                        fit: BoxFit.cover,
+                      )),
+                ],
+              );
             },
-            itemCount: transactions.length,
+          )
+        : ListView(
+            children: transactions
+                .map((tx) => TransactionItem(
+                  key: ValueKey(tx.id),
+                    transaction: tx, deleteTx: deleteTx))
+                .toList(),
+            
           );
   }
 }
-
