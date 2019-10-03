2da2a23159a445c52eab7a470b529332e338f976
mabirizi <mabiriziad@gmail.com>
2019-10-03 16:20:29 +0300

    extracting widgets

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 
 import '../models/transaction.dart';
+import './transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -32,45 +33,10 @@ class TransactionList extends StatelessWidget {
         },)
         : ListView.builder(
             itemBuilder: (ctx, index) {
-              return Card(
-                elevation: 10,
-                margin: EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Container(
-                      padding: EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text('${transactions[index].amount}/='),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    transactions[index].title,
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[index].date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 360
-                  ? FlatButton.icon(
-                    icon: Icon(Icons.delete),
-                    label: Text('Delete'),
-                    textColor: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transactions[index].id),
-                  )
-                  :IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transactions[index].id),
-                  ),
-                ),
-              );
+              return TransactionItem(transaction: transactions[index], deleteTx: deleteTx);
             },
             itemCount: transactions.length,
           );
   }
 }
+
