563c6f60f6ba73c0b725c19c9b1cec22964c1476
mabirizi <mabiriziad@gmail.com>
2019-10-03 12:43:44 +0300

    more cupertino styles

--- lib/main.dart
+++ lib/main.dart
@@ -158,7 +158,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('Show Chart'),
+                Text('Show Chart' , style: Theme.of(context).textTheme.title,),
                 Switch.adaptive(
                   activeColor: Theme.of(context).accentColor,
                   value: _showChart,
