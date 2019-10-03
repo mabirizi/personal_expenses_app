0da8f4c2be75d287b45fd81a2d017b830b104cba
mabirizi <mabiriziad@gmail.com>
2019-10-03 16:55:07 +0300

    problem with lists and stateful widgets while trying to set different colors for each circle avatar

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,9 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.transaction,
@@ -12,6 +13,29 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTx;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+
+  Color _bgColor;
+  
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.purple,
+    ];
+
+    _bgColor = availableColors[Random().nextInt(4)];
+
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,32 +47,33 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Container(
             padding: EdgeInsets.all(6),
             child: FittedBox(
-              child: Text('${transaction.amount}/='),
+              child: Text('${widget.transaction.amount}/='),
             ),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.title,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(transaction.date),
+          DateFormat.yMMMd().format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 360
         ? FlatButton.icon(
           icon: Icon(Icons.delete),
           label: Text('Delete'),
           textColor: Theme.of(context).errorColor,
-          onPressed: () => deleteTx(transaction.id),
+          onPressed: () => widget.deleteTx(widget.transaction.id),
         )
         :IconButton(
           icon: Icon(Icons.delete),
           color: Theme.of(context).errorColor,
-          onPressed: () => deleteTx(transaction.id),
+          onPressed: () => widget.deleteTx(widget.transaction.id),
         ),
       ),
     );
